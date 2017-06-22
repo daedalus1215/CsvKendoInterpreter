4c8640f19ee28584a89c2bcab51f71e6a5178413
larry <ladams1776@yahoo.com>
2017-06-22 10:15:24 -0400

    the beginning of doing a job is setup in index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ladams
+ * Date: 6/22/2017
+ * Time: 10:13 AM
+ */
+
+
+include(__DIR__.'\\vendor\\autoload.php');
+
+
+
+$interpreter = new \CsvKendoInterpreter\Interpreter();
